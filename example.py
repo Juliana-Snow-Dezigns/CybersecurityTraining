a91876bc6693dd94e0071f2dca5ccc59e96c1394
Juliana Snow Dezigns <92544710+Juliana-Snow-Dezigns@users.noreply.github.com>
2025-04-17 16:48:37 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+first_name = input ("Please type your name:")
+
+print(first_name)#not a string, but a variable
+
+age = input ("How old are you?")
+print(age)#not a string, but a variable
+
+#Hello Julaina
+print("Hello " + first_name)
+
+#f strings
+print(f"Hello {first_name} How are you doing today?")
