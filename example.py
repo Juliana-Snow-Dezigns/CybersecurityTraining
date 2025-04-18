a19ed9fb61158f1775697e45f7d06f1eea6d0985
Juliana Snow <92544710+Juliana-Snow-Dezigns@users.noreply.github.com>
2025-04-18 07:30:43 -0500

    Update example.py

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ age = input ("How old are you?")
 print(age)#not a string, but a variable
 
 #Hello Julaina
-print("Hello " + first_name)
+print('Hello ' + first_name)
 
 #f strings
-print(f"Hello {first_name} How are you doing today?")
+print(f'Hello {first_name} How are you {first_name}, {first_name} doing today?')
